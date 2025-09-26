deae2875c0281c40333fbf599643690b55d04b26
Benjamin Krause <b.krause@brain-scc.de>
2025-09-26 10:45:16 +0200

    Initial commit

---
+++ src/main/java/com/smartledore/pdf_utilities/PdfUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.smartledore.pdf_utilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfUtilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartledore/pdf_utilities/PdfUtilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartledore.pdf_utilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfUtilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
